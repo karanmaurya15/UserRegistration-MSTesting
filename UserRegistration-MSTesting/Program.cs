fae18a6fa226291175a5574d7f01002c65142976
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-19 16:41:48 +0530

    WEllcome

---
+++ UserRegistration-MSTesting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistration_MSTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to User Registration MS testing Program");
+        }
+    }
+}
